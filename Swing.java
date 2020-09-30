25297883bcd03727e3ed9f55204d0e3ff813b459
Googol <ronaldfleung@gmail.com>
2020-09-30 14:29:34 +0000

    JFrame 9/30/20

---
+++ Swing.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+class Swing {
+  public static void main(String[] args) {
+    // creating objects
+    JFrame window = new JFrame("Lab 4");
+    JPanel panel = new JPanel();
+    JButton bt1 = new JButton("Click Me!");
+  }
+}
