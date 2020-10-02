0816d57c88d82f9cc3d9fb434a5623cc5c072352
Googol <ronaldfleung@gmail.com>
2020-10-02 14:49:55 +0000

    test all color class constructors

--- unit2/SixColors.java
+++ unit2/SixColors.java
@@ -12,13 +12,20 @@ class SixColors {
     JPanel bottomCenter = new JPanel();
     JPanel bottomRight = new JPanel();
     GridLayout grid = new GridLayout(2, 3);
+    
+    Color x = new Color(153, 13, 255);
+    Color tr = new Color(138, 218, 222, 100);
+    Color y = new Color(0.5f, 0.6f, 0.85f);
+    Color ty = new Color(0.5f, 0.6f, 0.85f, 0.5f);
+    Color z = new Color(0x12D687);
+    Color tz = new Color(0x7912D687, true);
 
-    topLeft.setBackground(Color.RED);
-    topCenter.setBackground(Color.ORANGE);
-    topRight.setBackground(Color.YELLOW);
-    bottomLeft.setBackground(Color.GREEN);
-    bottomCenter.setBackground(Color.BLUE);
-    bottomRight.setBackground(Color.MAGENTA);
+    topLeft.setBackground(x);
+    topCenter.setBackground(tr);
+    topRight.setBackground(y);
+    bottomLeft.setBackground(ty);
+    bottomCenter.setBackground(z);
+    bottomRight.setBackground(tz);
 
     mainPanel.setLayout(grid);
     mainPanel.add(topLeft);
