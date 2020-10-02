0feff9d67e553c4f7ce21f9be89d42ba892a4728
Googol <ronaldfleung@gmail.com>
2020-10-02 18:13:06 +0000

    add labels to SixColors

--- unit2/SixColors.java
+++ unit2/SixColors.java
@@ -13,6 +13,7 @@ class SixColors {
     JPanel bottomRight = new JPanel();
     GridLayout grid = new GridLayout(2, 3);
     
+    // colors
     Color x = new Color(153, 13, 255);
     Color tr = new Color(138, 218, 222, 100);
     Color y = new Color(0.5f, 0.6f, 0.85f);
@@ -26,6 +27,38 @@ class SixColors {
     bottomLeft.setBackground(ty);
     bottomCenter.setBackground(z);
     bottomRight.setBackground(tz);
+    
+    // define labels
+    JLabel tlLab = new JLabel("153, 13, 255");
+    topLeft.add(tlLab);
+    JLabel tcLab = new JLabel("138, 218, 222, 100");
+    topCenter.add(tcLab);
+    JLabel trLab = new JLabel("0.5f, 0.6f, 0.85f");
+    topRight.add(trLab);
+    JLabel blLab = new JLabel("0.5f, 0.6f, 0.85f, 0.5f");
+    bottomLeft.add(blLab);
+    JLabel bcLab = new JLabel("0x12D687");
+    bottomCenter.add(bcLab);
+    JLabel brLab = new JLabel("0x7912D687, true");
+    bottomRight.add(brLab);
+
+    // label typeface
+    Font font = new Font("Serif", Font.BOLD, 18);
+    tlLab.setFont(font);
+    tcLab.setFont(font);
+    trLab.setFont(font);
+    blLab.setFont(font);
+    bcLab.setFont(font);
+    brLab.setFont(font);
+
+    // label color
+    Color txtColor = Color.RED.darker().darker();
+    tlLab.setForeground(txtColor);
+    tcLab.setForeground(txtColor);
+    trLab.setForeground(txtColor);
+    blLab.setForeground(txtColor);
+    bcLab.setForeground(txtColor);
+    brLab.setForeground(txtColor);
 
     mainPanel.setLayout(grid);
     mainPanel.add(topLeft);
