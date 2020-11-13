128efcf890e4b6d27301e6be3df8f0c772841d3b
Googol <ronaldfleung@gmail.com>
2020-11-13 19:50:01 +0000

    shorthand while loop

--- unit3/WhileLoopMethod.java
+++ unit3/WhileLoopMethod.java
@@ -1,8 +1,8 @@
 class WhileLoopMethod {
   public static void printUntilTarget(int min, int max, int target) {
     if (target >= min && target <= max) {
-      int randomInt;(int)
-      while (target != ((Math.random()*(max-min+1)) + min)) {
+      int randomInt;
+      while (target != (randomInt = (int)(Math.random()*(max-min+1))+min)) {
         System.out.println(randomInt);
       }
     }
