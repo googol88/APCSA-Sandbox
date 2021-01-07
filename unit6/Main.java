c72364e6e3673eda6a8f62e487a7c681ed5d0640
Googol <ronaldfleung@gmail.com>
2021-01-07 15:33:58 +0000

    notes on traversing arrays

--- unit6/Main.java
+++ unit6/Main.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 import java.awt.Color;
 
 class Main {
-  public static main(String[] args) {
+  public static void main(String[] args) {
     // declare array - fixed length and cannot change
     int[] favNumbers = {2, 4, 6, 8}; // {} IS NOT AN ARRAY! special syntax to build an array
-    String[] realStudents = new String[4];
-    booleanp[] bArr = new boolean[4];
+    String[] realStudents = new String[3];
+    boolean[] bArr = new boolean[4];
 
     // default values
     System.out.println(Arrays.toString(realStudents)); // [null, null, null, null]
@@ -17,7 +17,21 @@ class Main {
     realStudents[1] = "McStuffins";
     realStudents[2] = "Charles";
     realStudents[realStudents.length-1] = "The Queen"; // last index of array
-    realStudents[5] = "CJ"; // run-time error | OutOfBoundsException
+    //realStudents[7] = "CJ"; // run-time error | OutOfBoundsException
+    
+    
+    String[] fakeStudents = new String[4];
+    fakeStudents[0] = "bob";
+    fakeStudents[1] = "alice";
+    fakeStudents[3] = "dunkin";
+    for (String student : fakeStudents) {
+      System.out.println(student);
+    }
+    for (int i = 0; i < fakeStudents.length; i++) {
+      if(fakeStudents[i] != null)
+        fakeStudents[i] = fakeStudents[i].toUpperCase();
+      System.out.println(fakeStudents[i]);
+    }
 
     // creating aliases
     int[] myFavNumbers = favNumbers;
@@ -26,6 +40,7 @@ class Main {
     System.out.println(Arrays.toString(favNumbers)); // [1, 4, 6, 8]
     // same reference & memory location, so 
     // use clone method - favNumbers.clone()
+
 
     // print arguments in main method
     // java Main hello world goodbye -> [hello, world, goodbye]
