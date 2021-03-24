507c3231611578ccd1989e9cae7fa8a8c9fad95a
Googol <ronaldfleung@gmail.com>
2021-03-24 20:57:38 +0000

    add savings account problem

---
+++ unit9/SavingsAccount/Account.java
@@ -0,0 +1,67 @@
+public class Account
+{
+    private String name;
+    private double balance;
+
+    public Account(String name, double balance)
+    {
+       this.name = name;
+       this.balance = balance;
+    }
+
+    public String toString() {
+     return name + ", " + balance;
+    }
+
+    public boolean equals(Object other)
+    {
+       Account otherAccount = (Account) other;
+       return (this.balance == otherAccount.balance) &&
+                     this.name.equals(otherAccount.name);
+    }
+
+    public static void main(String[] args)
+    {
+        Account acct1 = new Account("Armani Smith",1500);
+                System.out.println(acct1);
+        // Uncomment this code to test SavingsAccount
+        
+        SavingsAccount acct2 = new SavingsAccount("Dakota Jones",1500,4.5);
+        SavingsAccount acct3 = new SavingsAccount("Dakota Jones",1500,4.5);
+                System.out.println(acct2);
+                System.out.println(acct2.equals(acct3));
+        
+    }
+}
+/* Write the SavingsAccount class which inherits from Account
+   and has an interest rate and a constructor, toString, and
+   equals methods.
+*/
+class SavingsAccount extends Account
+{
+  private String name;
+  private double balance;
+  private double interestRate;
+
+  public SavingsAccount(String name, double balance, double interestRate) {
+    super(name, balance);
+    this.name = name;
+    this.balance = balance;
+    this.interestRate = interestRate;
+  }
+
+  public String toString() {
+    return super.toString() + ", " + interestRate;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (other instanceof SavingsAccount) {
+      SavingsAccount otherAccount = (SavingsAccount) other;
+      return (this.balance == otherAccount.balance) &&
+              this.interestRate == otherAccount.interestRate &&
+              this.name.equals(otherAccount.name);
+    }
+    return false;
+  }
+}
---
+++ unit9/SavingsAccount/README.md
@@ -0,0 +1,14 @@
+# 9.7.4. groupwork Programming Challenge: Savings Account
+In the following code, a bank account class contains the account holder’s name and the money balance in the account.
+
+Work in pairs to write the following code and test each part before moving on to the next step:
+
+1. Write a subclass called SavingsAccount that extends Account and adds an interest rate variable.
+
+2. Write a constructor with 3 arguments (name, balance, interest rate) for the SavingsAccount class that uses the super constructor.
+
+3. Write a toString() method for SavingsAccount that returns a call to the super toString() method and the interest rate.
+
+4. Write an equals method for SavingsAccount that calls the superclass equals method and checks that the interest rates are equal.
+
+Complete the subclass SavingsAccount below which inherits from Account and adds an interest rate variable. Write a constructor with 3 arguments, a toString, and an equals method for it. Uncomment the code in main to test your new class and methods.
